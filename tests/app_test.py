1d74a358df4ab2f326e15f7cc16d75580f981063
Alexis Murari <alexis.murari@mail.utoronto.ca>
2021-10-31 13:17:15 -0400

    Completed the activity

--- tests/app_test.py
+++ tests/app_test.py
@@ -23,7 +23,6 @@ def client():
     db.drop_all()  # teardown
 
 
-
 def login(client, username, password):
     """Login helper function"""
     return client.post(
@@ -37,9 +36,10 @@ def logout(client):
     """Logout helper function"""
     return client.get("/logout", follow_redirects=True)
 
+
 def search(query):
     """Search helper function"""
-    return ("/search/?query="+query)
+    return "/search/?query=" + query
 
 
 def test_index(client):
@@ -83,6 +83,7 @@ def test_messages(client):
     assert b"&lt;Hello&gt;" in rv.data
     assert b"<strong>HTML</strong> allowed here" in rv.data
 
+
 def test_delete_message(client):
     """Ensure the messages are being deleted"""
     rv = client.get("/delete/1")
@@ -92,7 +93,6 @@ def test_delete_message(client):
     rv = client.get("/delete/1")
     data = json.loads(rv.data)
     assert data["status"] == 1
-
 
 
 def test_search(client):
